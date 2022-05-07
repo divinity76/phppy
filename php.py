7af070d18e7c23db452839f7236ae92222f3584d
divinity76 <divinity76@gmail.com>
2022-05-07 12:45:40 +0200

    Update php.py

--- php.py
+++ php.py
@@ -5,8 +5,7 @@ import sys
 def echo(*args: str)->None:
     for arg in args:
         sys.stdout.buffer.write(arg.encode("raw_unicode_escape"));
-
-
+    #sys.stdout.flush();
 
 def strpos(haystack: int, needle: str, offset: int = 0)-> int|None:
     # cheat sheet: https://stackoverflow.com/a/17143335/1067003
@@ -19,6 +18,6 @@ def strpos(haystack: int, needle: str, offset: int = 0)-> int|None:
 def escapeshellarg(s: str)->str:
     #Todo: escapeshellarg for windows? it's hell though: https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
     # this is the bash method: 
-    if b'\x00' in s:
+    if s.find("\x00") != -1:
         raise ValueError("String contains null bytes, it is impossible to escape null bytes in bash!");
     return "'" + s.replace("'", "'\\''") + "'"
