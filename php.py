f6584f4bb92fc860cb57c7df5fd2218bf4112580
divinity76 <divinity76@gmail.com>
2022-05-07 10:00:44 +0200

    Create php.py

---
+++ php.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+
+def echo(*args: str)->None:
+    for arg in args:
+        print(arg);
+
+
+def strpos(haystack: int, needle: str, offset: int = 0)-> int|None:
+    # cheat sheet: https://stackoverflow.com/a/17143335/1067003
+    pos = haystack.find(needle, offset);
+    if pos == -1:
+        return None;
+    return pos;
+
+
+def escapeshellarg(s: str)->str:
+    #Todo: escapeshellarg for windows? it's hell though: https://docs.microsoft.com/en-us/archive/blogs/twistylittlepassagesallalike/everyone-quotes-command-line-arguments-the-wrong-way
+    # this is the bash method: 
+    if b'\x00' in s:
+        raise ValueError("String contains null bytes, it is impossible to escape null bytes in bash!");
+    return "'" + s.replace("'", "'\\''") + "'"
